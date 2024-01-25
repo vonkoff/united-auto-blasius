e7db353686345b4928fe29c833be9f9efeb5507f
Ivan Vukusic <jaaajaa102@gmail.com>
2024-01-24 20:00:49 -0500

    remove api reference trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { api } from "~/trpc/server";
 
 export default async function Home() {
   noStore();
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  // const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -39,29 +39,25 @@ export default async function Home() {
           </Link>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+          <p className="text-2xl text-white">hello</p>
         </div>
-
-        <CrudShowcase />
       </div>
     </main>
   );
 }
 
 async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
+  // const latestPost = await api.post.getLatest.query();
+  //
+  // return (
+  //   <div className="w-full max-w-xs">
+  //     {latestPost ? (
+  //       <p className="truncate">Your most recent post: {latestPost.name}</p>
+  //     ) : (
+  //       <p>You have no posts yet.</p>
+  //     )}
+  //
+  //     <CreatePost />
+  //   </div>
+  // );
 }
