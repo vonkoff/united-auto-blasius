9d82ac29f57c1a52b9c6a0290fcb03a07ff24893
Ivan Vukusic <jaaajaa102@gmail.com>
2024-09-27 14:13:09 -0400

    Update map size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,6 +29,7 @@ export default async function Home() {
             <div className="absolute inset-0">
               <Image
                 className="h-full w-full object-cover"
+                priority={true}
                 src={frontUnitedAuto}
                 alt="Front of United Auto building"
                 width={500}
@@ -91,7 +92,7 @@ export default async function Home() {
       <div className="relative">
         <div className="mx-auto grid h-1/2 h-full max-h-fit grid-cols-1 items-center justify-center justify-items-start bg-black md:grid-cols-2 md:justify-items-center">
           <div className="mx-2 mb-3 flex flex-col justify-center gap-1 pl-4 text-white md:gap-4 md:pl-0">
-            <h2 className="mb-1 mt-4 text-xl uppercase md:mx-auto lg:text-2xl">
+            <h2 className="mb-1 mt-4 text-xl uppercase md:mx-auto md:mt-0 lg:text-2xl">
               Get Directions
             </h2>
             <a
@@ -111,21 +112,21 @@ export default async function Home() {
                   <u>
                     <strong>Sales</strong>
                   </u>
-                  : 9am-5pm
+                  : 8am-5pm
                 </p>
                 <p>
                   Open Today!{" "}
                   <u>
                     <strong>Service</strong>
                   </u>
-                  : 9am-5pm
+                  : 8am-5pm
                 </p>
                 <p>
                   Saturday Only!{" "}
                   <u>
                     <strong>Emissions</strong>
                   </u>
-                  : 9am-12pm
+                  : 8am-1pm
                 </p>
               </div>
             </span>
@@ -136,12 +137,15 @@ export default async function Home() {
             rel="noopener noreferrer"
             className="order-first w-full md:order-last"
           >
-            <Image
-              className="object-fit max-h-64 w-full cursor-pointer"
-              alt="map location of United Auto"
-              src={mapPic}
-              priority={true}
-            />
+            <div className="relative h-0 w-full pb-[75%]">
+              <Image
+                className="absolute inset-0 h-full w-full object-cover"
+                alt="map location of United Auto"
+                src={mapPic}
+                priority={true}
+                layout="fill"
+              />
+            </div>
           </a>
         </div>
       </div>
